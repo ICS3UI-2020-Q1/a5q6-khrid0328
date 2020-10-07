ed306060a6648ab7059c351b58973c1d643d271b
khrid0328 <khrid0328@wrdsb.ca>
2020-10-07 02:38:20 +0000

    A5Q6 This program prints a box of stars depends on the users input

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program prints a box of stars depends on the users input
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
     
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter a number between 1 and 10
+    System.out.println("Please enter a number between 1 and 10");    
+
+    // create a variable for user input
+    int number = input.nextInt();
+
+    // use a for loop to draw a line of *
+    for (int count =1; count <= number; count++){
+    
+      // use a for loop to draw a line of *
+      for(int i = 1; i <= number; i++){
+
+        // loop action: print *
+        System.out.print("* ");
+    
+      }
+
+      // create a blank line
+      System.out.println("");
+    }
   }
 }
